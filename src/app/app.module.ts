1eddc6096822c34392685cd745273166b7677398
alex-lacroix <alex.lac@live.ca>
2020-02-02 15:58:32 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { OfferedServicesComponent } from './offered-services/offered-services.component';
+import { InvestingComponent } from './investing/investing.component';
+import { TestimonyComponent } from './testimony/testimony.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent, NavbarComponent, AboutComponent, OfferedServicesComponent, InvestingComponent, TestimonyComponent, ContactComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
