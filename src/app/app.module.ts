db8c9f672a170645a075761c158a5b352c85ce63
alex-lacroix <alex.lac@live.ca>
2020-02-02 17:50:43 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,24 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
-import { NavbarComponent } from './navbar/navbar.component';
-import { AboutComponent } from './about/about.component';
-import { OfferedServicesComponent } from './offered-services/offered-services.component';
-import { InvestingComponent } from './investing/investing.component';
-import { TestimonyComponent } from './testimony/testimony.component';
-import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { AboutComponent } from "./about/about.component";
+import { OfferedServicesComponent } from "./offered-services/offered-services.component";
+import { InvestingComponent } from "./investing/investing.component";
+import { TestimonyComponent } from "./testimony/testimony.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavbarComponent, AboutComponent, OfferedServicesComponent, InvestingComponent, TestimonyComponent, ContactComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    OfferedServicesComponent,
+    InvestingComponent,
+    TestimonyComponent,
+    ContactComponent
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
