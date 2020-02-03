b59b924454a857cdecbf4b4ae8a052f776dcf22e
alex-lacroix <alex.lac@live.ca>
2020-02-03 17:42:39 -0500

    commit

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  scrollToContact() {
+    let elem = document.getElementById("contact");
+    elem.scrollIntoView({ block: "end", behavior: "smooth" });
   }
 
+  scrollToHome() {
+    let elem = document.getElementById("home");
+    elem.scrollIntoView({ block: "end", behavior: "smooth" });
+  }
+
+  scrollToAbout() {
+    let elem = document.getElementById("about");
+    elem.scrollIntoView({ block: "end", behavior: "smooth" });
+  }
+
+  scrollToServices() {
+    let elem = document.getElementById("services");
+    elem.scrollIntoView({ block: "end", behavior: "smooth" });
+  }
+
+  scrollToTestimonies() {
+    let elem = document.getElementById("testimony-carousel");
+    elem.scrollIntoView({ block: "end", behavior: "smooth" });
+  }
 }
